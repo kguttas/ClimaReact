63d17df19aaae87e36f53cc9ae2de0128798c42f
Mauricio Estrada Celis <kguttas@hotmail.com>
2018-12-07 13:48:10 -0300

    primer commit

---
+++ precache-manifest.57b8a53b81f12f74f5f7e25acf461735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb0c7fe78bbd020a3d42",
+    "url": "/static/js/main.eb0c7fe7.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "eb0c7fe78bbd020a3d42",
+    "url": "/static/css/main.00bfe1ac.chunk.css"
+  },
+  {
+    "revision": "55a93b0e63bec3b47774e6246768c576",
+    "url": "/index.html"
+  }
+];
